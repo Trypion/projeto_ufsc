deda2c7d945bbf16b719c1300013656361e1fc80
Israel <israel.schmitt.j@gmail.com>
2021-08-01 12:34:07 -0300

    adicionado cors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from src.routes.university import UniversityRoutes
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+cors = CORS(app, expose_headers=[
+            "Content-Disposition", "Access-Control-Allow-Origin"])
 
 university_routes = UniversityRoutes()
 app.register_blueprint(university_routes, url_prefix='/university')
