d8fafe55b29a40349fe119aa1881db89f36a8e47
leessandro <lissandrorezende@yahoo.com.br>
2021-08-10 01:59:47 -0300

    commit

--- src/controllers/profile.py
+++ src/controllers/profile.py
@@ -5,23 +5,26 @@ from src.models.profile import Profile
 from src.controllers.errors.user_not_found import UserNotFound
 from src.controllers.user import UserController
 from src.controllers.university import UniversityController
-from src.controllers.course import Course
+from src.controllers.course import Course, CourseController
 
 
 class ProfileController(Controller):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, user_controller : UserController, university_controller : UniversityController, course_controller : CourseController) -> None:
         self.__profiles = []
-        self.__university_controller = UniversityController()
-        self.__user_controller = UserController()
-        self.__user_courses = Course()
+        self.__user_controller = user_controller
+        self.__university_controller = university_controller
+        self.__course_controller = course_controller
 
-    def create(self, name: str, email: str, sex: str, age: int, university_id: str, profile_picture: str, university_register: str, course_id: str, ranking: int, user: str):
-
+    def create(self, name: str, email: str, sex: str, age: int, university_id: str, profile_picture: str, university_register: str, course_id: str, ranking: int, user : str):
+        self.__user_controller.find(user)
+        self.__university_controller.find(university_id)
+        self.__course_controller.find(course_id)
+        
         id = str(uuid4())
         profile = Profile(id, name, email, sex, age, university_id,
                           profile_picture, university_register, course_id, ranking, user)
         self.__profiles.append(profile)
+        return id
 
 
 
@@ -37,15 +40,16 @@ class ProfileController(Controller):
         profile = self.find_by_id(id, self.__profiles)
         if not profile:
             return
-        name = name
-        email = email
-        sex = sex
-        university_id = university_id
-        profile_picture = profile_picture
-        university_register = university_register
-        course_id = course_id
-        ranking = ranking
+        profile.name = name
+        profile.email = email
+        profile.sex = sex
+        profile.university_id = university_id
+        profile.profile_picture = profile_picture
+        profile.university_register = university_register
+        profile.course_id = course_id
+        profile.ranking = ranking
         user = user
+        profile.updated_at = str(datetime.now())
         return profile.as_dict()
 
     def delete(self, id, user) -> str:
