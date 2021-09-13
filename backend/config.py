f2ec93b67c54c8b999981fdef7510172c388b04d
Israel <israel.schmitt.j@gmail.com>
2021-09-12 21:08:24 -0300

    config com variavel de ambiente

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
-SECRET_KEY = "NMgh\x98\x87zrqj\xde\x8b\x81\xf8c|u\x84\x119v\xb4}\xc1"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "NMgh\x98\x87zrqj\xde\x8b\x81\xf8c|u\x84\x119v\xb4}\xc1")
