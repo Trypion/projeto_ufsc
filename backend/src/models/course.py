be3085c13b87389ce3f25f6e65a56f1ba6ac5aa3
leessandro <lissandrorezende@yahoo.com.br>
2021-09-18 16:26:40 -0300

    course model changed

--- backend/src/models/course.py
+++ backend/src/models/course.py
@@ -1,3 +1,4 @@
+from bson.objectid import ObjectId
 from src.models.university import University
 from src.models.timestamp import Timestamp
 from src.models.user import User
@@ -6,7 +7,7 @@ from datetime import datetime
 
 
 class Course(Timestamp):
-    def __init__(self, id: str, name: str, university: University, user: User, ranking: int = 0) -> None:
+    def __init__(self, id: str, name: str, university: ObjectId, user: User, ranking: int = 0) -> None:
         super().__init__()
         self.__id = id
         self.__name = name
@@ -15,18 +16,34 @@ class Course(Timestamp):
         self.created_at = datetime.now()
         self.created_by = user
 
+    def serialize(self):
+        return {
+            'id': self.__id,
+            'name': self.__name,
+            'university': self.__university,
+            'ranking': self.__ranking,
+            'created_by': self.created_by,
+            'created_at': self.created_at,
+            'updated_by': self.updated_by,
+            'updated_at': self.updated_at,
+            'deleted_by': self.deleted_by,
+            'deleted_at': self.deleted_at
+        }
+
+
+
     def as_dict(self):
         return {
             'id': self.__id,
             'name': self.__name,
             'university': self.__university.as_dict(),
             'ranking': self.__ranking,
-            'created_by': self.created_by.as_dict(),
-            'created_at': datetime.strftime(self.created_at, "%d/%m/%Y"),
-            'updated_by': self.updated_by.as_dict() if self.updated_by else None,
-            'updated_at': datetime.strftime(self.updated_at, "%d/%m/%Y") if self.updated_at else None,
-            'deleted_by': self.deleted_by.as_dict() if self.deleted_by else None,
-            'deleted_at': datetime.strftime(self.deleted_at, "%d/%m/%Y") if self.deleted_at else None,
+            'created_by': str(self.created_by),
+            'created_at': self.created_at,
+            'updated_by': str(self.updated_by),
+            'updated_at': self.updated_at,
+            'deleted_by': str(self.deleted_by),
+            'deleted_at': self.deleted_at
         }
 
     @property
