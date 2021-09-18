9c8bb8c141662e2f0cf57038c32d0d507690e356
Israel <israel.schmitt.j@gmail.com>
2021-09-18 16:40:49 -0300

    profile e universidades

---
+++ frontend/src/app/models/profile.model.ts
@@ -0,0 +1,13 @@
+export interface Profile {
+  id: string
+  name: string
+  email: string
+  sex: string
+  age: number
+  university: string
+  profile_picture: string
+  university_register: string
+  course: string
+  ranking: number
+  user: string
+}
